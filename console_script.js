8f73b8d861f2c15cb73a73e8644f069c2d4470a4
Sutas Nakasawek <sutas@arterex.com>
2018-06-20 14:50:19 +0700

    1.2 add filtering, grouping add matching for other language than eng, rus add loading text

--- console_script.js
+++ console_script.js
@@ -2,7 +2,27 @@
     const read_data = () => {
         const player_name = document.getElementsByClassName("profile_small_header_name")[0].children[0].textContent;
         const row_list = document.getElementById("personaldata_elements_container").querySelectorAll("tr");
-        const yes_marks = ["Yes", "Да"];
+        const yes_marks = [
+            "Yes",
+            "Да",
+            "Ano",
+            "Ja",
+            "Kyllä",
+            "Oui",
+            "Ναι",
+            "Igen",
+            "Sì",
+            "はい",
+            "예",
+            "Tak",
+            "Sim",
+            "Da",
+            "是",
+            "Sí",
+            "ใช่",
+            "Evet",
+            "Так",
+        ];
         let all_output = player_name + ",";
         for (let i = 1; i < row_list.length; i++) {
             const col_list = row_list[i].querySelectorAll("td");
@@ -23,10 +43,10 @@
     const load_more = () => b1.click();
     const watch_load_more = () => {
         const interval = setInterval(() => {
-            if(has_load_more()) {
+            if (has_load_more()) {
                 load_more();
             } else {
-                if(!has_more()) {
+                if (!has_more()) {
                     clearInterval(interval);
                     console.log(`done`);
                     read_data();
@@ -34,5 +54,14 @@
             }
         }, 100);
     };
+
+    const b3 = document.getElementById("personaldata_elements_container");
+    const h1 = document.createElement("h1");
+    const h1text = document.createTextNode("Loading more data ");
+    const h1spinner = document.createElement("img");
+    h1spinner.src = "https://steamcommunity-a.akamaihd.net/public/images/login/throbber.gif";
+    h1.appendChild(h1text);
+    h1.appendChild(h1spinner);
+    b3.insertBefore(h1, b3.childNodes[0]);
     watch_load_more();
 })()
